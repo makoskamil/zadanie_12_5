b6a946f97ee792a4afade49144ebfc61b69a2cb2
Kamil Makoś <makos.kamil@o2.pl>
2018-04-11 14:05:18 +0200

    changeJS

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function getQuote() {
 }
 
 function createTweet(input) {
-    var data = input[0];
+    var data = input[1];
     var quoteText = $(data.content).text().trim();
     var quoteAuthor = data.title;
     
